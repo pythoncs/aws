2780526332638266bbe6a70fa5d9d1e57143309a
Cui_Shu <15630021269@163.com>
2020-09-01 18:26:14 +0800

    修改main.py 添加题库总数

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from flask_restful import Resource, Api
 import json
 import xlrd
 
-
 app = Flask(__name__)
 api = Api(app)
 
 
 class AwsModel(Resource):
 
-    def read_excel_data(self,filepath):
+    def read_excel_data(self, filepath):
         # 读取Excel文件
         workbook = xlrd.open_workbook(filepath)
         # 获取sheet1 单选题库
@@ -29,27 +28,21 @@ class AwsModel(Resource):
             option_list.append('B. ' + row_value[2])
             option_list.append('C. ' + row_value[3])
             option_list.append('D. ' + row_value[4])
-            # question_dict['A'] = row_value[1]
-            # question_dict['B'] = row_value[2]
-            # question_dict['C'] = row_value[3]
-            # question_dict['D'] = row_value[4]
             question_dict['option'] = option_list
             question_dict['answer'] = row_value[5]
             question_list.append(question_dict)
-
-        print(len(question_list))
-        return question_list
+        total_number = len(question_list)
+        return question_list, total_number
 
     def post(self):
         # 获取json数据
         res = request.json
         try:
             if res['TOKEN'] == 'aws-sap-cs':
-
-                question_list = self.read_excel_data('aws.xlsx')
+                question_list, total_number = self.read_excel_data('aws.xlsx')
                 response = {
-                    'data': question_list[int(res['page'])]
-                    # 'data': question_list
+                    'data': question_list[int(res['page'])],
+                    'total_number': total_number
                 }
                 return response
 
